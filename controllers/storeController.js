25e8a041d7eff508ea4c141deaee5790f7a248b3
Alex <alex.nielsen@gmail.com>
2018-05-17 22:16:04 -0400

    finished 13, start on creating and editing flow for stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,14 +1,27 @@
+const mongoose = require("mongoose");
+const Store = mongoose.model("Store");
 
 exports.homePage = (req, res) => {
-    res.render('index', {name: req.name})
-}
 
-exports.addStore= (req,res) => {
-    res.render('editStore', {
-        title: "Add store"
-    })
-}
+  res.render("index", { name: req.name });
+};
 
-exports.createStore = (req, res) => {
-res.json(req.body)
+exports.addStore = (req, res) => {
+  res.render("editStore", {
+    title: "Add store"
+  });
+};
+
+exports.createStore = async (req, res) => {
+  const store = await (new Store(req.body)).save();
+  await store.save();
+  req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`)
+  console.log('it worked')
+};
+
+exports.getStores = async (req, res) => {
+  // get all stores from database
+  const stores = await Store.find();
+  res.render( 'stores', { title: 'Stores', stores: stores})
 }
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const storeController = require('../controllers/storeController')
+const storeController = require("../controllers/storeController");
 // Do work here
-router.get('/',  storeController.homePage);
-router.get('/add',  storeController.addStore);
-router.post('/add', storeController.createStore)
 
-
-
+const { catchErrors } = require("../handlers/errorHandlers");
+router.get("/", catchErrors(storeController.getStores));
+router.get("/stores", storeController.getStores);
+router.get("/add", storeController.addStore);
+router.post("/add", catchErrors(storeController.createStore));
 
 module.exports = router;
